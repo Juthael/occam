6b97751b1dbff65d570a7a40fb409d6366e046a8
Tregouet <gael.tregouet@gmail.com>
2021-07-26 16:50:04 +0200

    initial

---
+++ src/main/java/com/tregouet/occam/App.java
@@ -0,0 +1,13 @@
+package com.tregouet.occam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

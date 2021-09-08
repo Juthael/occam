2cb5533519796c781544fcdc264aa4d2cd66dca0
gael-tregouet <gael.tregouet@gmail.com>
2021-09-08 15:15:11 +0200

    ConjunctiveOperator

---
+++ src/main/java/com/tregouet/occam/data/operators/IConjunctiveOperator.java
@@ -0,0 +1,7 @@
+package com.tregouet.occam.data.operators;
+
+public interface IConjunctiveOperator extends IOperator {
+	
+	boolean addOperator(IOperator operator);
+
+}
--- src/main/java/com/tregouet/occam/transition_function/impl/State.java
+++ src/main/java/com/tregouet/occam/transition_function/impl/State.java
@@ -124,4 +124,35 @@ public class State implements IState {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		return true;
+	}
+
 }

381bc5eecbfdc78bfa943f1839707a9f6292b804
Gael Tregouet <TREGOUET@DESKTOP-PPR7NPG>
2022-04-19 14:33:07 +0200

    denotation building test

--- src/test/java/com/tregouet/occam/alg/builders/representations/productions/from_denotations/impl/MapTargetVarsToSourceValuesTest.java
+++ src/test/java/com/tregouet/occam/alg/builders/representations/productions/from_denotations/impl/MapTargetVarsToSourceValuesTest.java
@@ -1,9 +1,11 @@
 package com.tregouet.occam.alg.builders.representations.productions.from_denotations.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,9 +14,10 @@ import org.junit.Test;
 import com.tregouet.occam.data.logical_structures.languages.alphabets.ISymbol;
 import com.tregouet.occam.data.logical_structures.languages.alphabets.impl.Terminal;
 import com.tregouet.occam.data.logical_structures.languages.alphabets.impl.Variable;
-import com.tregouet.occam.data.logical_structures.languages.words.construct.IConstruct;
 import com.tregouet.occam.data.logical_structures.languages.words.construct.impl.Construct;
 import com.tregouet.occam.data.representations.concepts.denotations.IDenotation;
+import com.tregouet.occam.data.representations.concepts.denotations.impl.Denotation;
+import com.tregouet.occam.data.representations.transitions.productions.IContextualizedProduction;
 
 public class MapTargetVarsToSourceValuesTest {
 	
@@ -29,21 +32,70 @@ public class MapTargetVarsToSourceValuesTest {
 
 	@Before
 	public void setUp() throws Exception {
+		alphaBetaGamma = array2Denotation(new String[] {"alpha", "beta", "gamma"}, 0);
+		alphaDeltaEpsilonZeta = array2Denotation(new String[] {"alpha", "delta", "epsilon", "zeta"}, 0);
+		alphaXGamma = array2Denotation(new String[] {"alpha", "var", "gamma"}, 1);
+		alphaXEpsilonX = array2Denotation(new String[] {"alpha", "var", "epsilon", "var"}, 1);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void whenProductionsRequestedThenExpectedNumberReturned() {
+		boolean asExpected = true;
+		if (howManyProductionsFor(alphaBetaGamma, alphaDeltaEpsilonZeta) != 0)
+			asExpected = false;
+		if (howManyProductionsFor(alphaXGamma, alphaBetaGamma) != 0)
+			asExpected = false;
+		if (howManyProductionsFor(alphaBetaGamma, alphaXGamma) != 1)
+			asExpected = false;
+		if (howManyProductionsFor(alphaXEpsilonX, alphaDeltaEpsilonZeta) != 0)
+			asExpected = false;
+		if (howManyProductionsFor(alphaDeltaEpsilonZeta, alphaXEpsilonX) != 2)
+			asExpected = false;		
+		if (howManyProductionsFor(alphaXEpsilonX, alphaBetaGamma) != 0)
+			asExpected = false;
+		if (howManyProductionsFor(alphaDeltaEpsilonZeta, alphaDeltaEpsilonZeta) != 1)
+			asExpected = false;
+		assertTrue(asExpected);
 	}
 	
-	private IConstruct array2Construct(String[] array) {
+	private int howManyProductionsFor(IDenotation source, IDenotation target) {
+		MapTargetVarsToSourceValues prodBuilder = new MapTargetVarsToSourceValues();
+		Set<IContextualizedProduction> productions = prodBuilder.apply(source, target);
+		/*
+		System.out.println(report(source, target, productions));
+		*/
+		return productions.size();
+	}
+	
+	@SuppressWarnings("unused")
+	private String report(IDenotation source, IDenotation target, Set<IContextualizedProduction> result) {
+		String nL = System.lineSeparator();
+		if (result.isEmpty())
+			return "No set of productions can generate [" + source.toString() + "]" + nL +  
+					"out of [" + target.toString() + "]." + nL;
+		else {
+			StringBuilder sB = new StringBuilder();
+			sB.append("[" + source.toString() + "] can be generated out of [" + target.toString() + "]" 
+				+ nL + "by application of the following productions : " + nL);
+			Iterator<IContextualizedProduction> prodIte = result.iterator();
+			while (prodIte.hasNext()) {
+				sB.append("   " + prodIte.next().toString());
+				if (prodIte.hasNext())
+					sB.append(nL);
+			}
+			sB.append(nL);
+			return sB.toString();
+		}
+	}
+	
+	private IDenotation array2Denotation(String[] array, int conceptID) {
 		List<ISymbol> symbols = new ArrayList<>();
 		for (String string : array) {
 			if (string.equals("var"))
 				symbols.add(new Variable(false));
 			else symbols.add(new Terminal(string));
 		}
-		return new Construct(symbols);
+		return new Denotation(new Construct(symbols), conceptID);
 	}
 
 }
---
+++ src/test/java/com/tregouet/occam/alg/builders/representations/productions/impl/IfIsAThenBuildProductionsTest.java
@@ -0,0 +1,24 @@
+package com.tregouet.occam.alg.builders.representations.productions.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IfIsAThenBuildProductionsTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

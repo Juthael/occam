78316f9cde04659ee11088941c70b995538a9de3
Gael Tregouet <TREGOUET@DESKTOP-PPR7NPG>
2022-06-11 17:43:33 +0200

    debug

--- src/main/java/com/tregouet/occam/data/problem_space/states/descriptions/differentiae/properties/applications/impl/OmegaApplication.java
+++ src/main/java/com/tregouet/occam/data/problem_space/states/descriptions/differentiae/properties/applications/impl/OmegaApplication.java
@@ -3,6 +3,8 @@ package com.tregouet.occam.data.problem_space.states.descriptions.differentiae.p
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.tregouet.occam.data.problem_space.states.classifications.concepts.denotations.impl.Denotation;
+import com.tregouet.occam.data.problem_space.states.classifications.concepts.impl.Concept;
 import com.tregouet.occam.data.problem_space.states.descriptions.differentiae.properties.applications.IApplication;
 import com.tregouet.occam.data.problem_space.states.productions.IProduction;
 import com.tregouet.occam.data.problem_space.states.productions.impl.OmegaProd;
@@ -12,7 +14,8 @@ public class OmegaApplication extends Application implements IApplication {
 	public static final OmegaApplication INSTANCE = new OmegaApplication();
 
 	private OmegaApplication() {
-		super(null, null, new ArrayList<>(Arrays.asList(new IProduction[] {OmegaProd.INSTANCE})), null);
+		super(null, null, new ArrayList<>(Arrays.asList(new IProduction[] {OmegaProd.INSTANCE})), 
+				new Denotation(OmegaProd.INSTANCE.getValue(), Concept.WHAT_IS_THERE_ID));
 	}
 
 }

76ac8697d4bd5e0b04a6a92428fe08802e595e51
Gael Tregouet <TREGOUET@192.168.0.16>
2022-06-19 06:17:13 +0200

    unused class deletion

--- src/main/java/com/tregouet/occam/alg/builders/pb_space/ProblemSpaceExplorerFactory.java
+++ src/main/java/com/tregouet/occam/alg/builders/pb_space/ProblemSpaceExplorerFactory.java
@@ -1,10 +1,6 @@
 package com.tregouet.occam.alg.builders.pb_space;
 
-import com.tregouet.occam.alg.builders.pb_space.impl.AutomaticallyExpandTrivialLeaves;
 import com.tregouet.occam.alg.builders.pb_space.impl.NormalizeClassificationThenBuildProductions;
-import com.tregouet.occam.alg.builders.pb_space.impl.RemoveMeaningless;
-import com.tregouet.occam.alg.builders.pb_space.impl.RemoveUninformative;
-import com.tregouet.occam.alg.builders.pb_space.impl.SparseReducer;
 
 public class ProblemSpaceExplorerFactory {
 
@@ -15,16 +11,8 @@ public class ProblemSpaceExplorerFactory {
 
 	public ProblemSpaceExplorer apply(ProblemSpaceExplorerStrategy strategy) {
 		switch (strategy) {
-		case REMOVE_MEANINGLESS :
-			return new RemoveMeaningless();
-		case REMOVE_UNINFORMATIVE :
-			return new RemoveUninformative();
-		case EXPAND_TRIVIAL_LEAVES :
-			return new AutomaticallyExpandTrivialLeaves();
 		case NORMALIZE_CLASS_THEN_BUILD :
 			return new NormalizeClassificationThenBuildProductions();
-		case SPARSE_REDUCER : 
-			return new SparseReducer();
 		default :
 			return null;
 		}

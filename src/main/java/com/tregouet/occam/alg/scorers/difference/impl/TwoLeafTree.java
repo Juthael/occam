5306af52fe8025cf33b8749750a2fc6a1923bc82
Gael Tregouet <TREGOUET@192.168.0.16>
2022-06-24 10:14:24 +0200

    difference scorer test & debug

--- src/main/java/com/tregouet/occam/alg/scorers/difference/impl/TwoLeafTree.java
+++ src/main/java/com/tregouet/occam/alg/scorers/difference/impl/TwoLeafTree.java
@@ -1,6 +1,7 @@
 package com.tregouet.occam.alg.scorers.difference.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -8,10 +9,13 @@ import java.util.Set;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 
 import com.tregouet.occam.alg.scorers.difference.DifferenceScorer;
+import com.tregouet.occam.data.logical_structures.languages.words.construct.IConstruct;
 import com.tregouet.occam.data.problem_space.states.classifications.IClassification;
 import com.tregouet.occam.data.problem_space.states.classifications.concepts.IConcept;
 import com.tregouet.occam.data.problem_space.states.classifications.concepts.IConceptLattice;
 import com.tregouet.occam.data.problem_space.states.classifications.concepts.IIsA;
+import com.tregouet.occam.data.problem_space.states.classifications.concepts.denotations.IDenotation;
+import com.tregouet.occam.data.problem_space.states.classifications.concepts.impl.Concept;
 import com.tregouet.occam.data.problem_space.states.classifications.concepts.impl.IsA;
 import com.tregouet.occam.data.problem_space.states.descriptions.IDescription;
 import com.tregouet.occam.data.problem_space.states.descriptions.differentiae.ADifferentiae;
@@ -42,6 +46,9 @@ public class TwoLeafTree implements DifferenceScorer {
 	}
 	
 	private InvertedTree<IConcept, IIsA> setUpClassificationTree(int particularID1, int particularID2, IConceptLattice lattice) {
+		Set<Integer> extentIDs = new HashSet<>();
+		extentIDs.add(particularID1);
+		extentIDs.add(particularID2);
 		IConcept particular1 = null;
 		IConcept particular2 = null;
 		//retrieve vertices
@@ -51,31 +58,39 @@ public class TwoLeafTree implements DifferenceScorer {
 			else if (particular.iD() == particularID2)
 				particular2 = particular;
 		}
-		IConcept truism = lattice.getTruism();
-		IConcept ontologicalCommitment = lattice.getOntologicalCommitment();
-		IConcept genus = Functions.supremum(lattice.getOntologicalUpperSemilattice(), particular1, particular2);
+		IConcept ontologicalCommitment = copyConceptWithRestrictedExtent(lattice.getOntologicalCommitment(), extentIDs);
+		IConcept genus = copyConceptWithRestrictedExtent(
+				Functions.supremum(lattice.getOntologicalUpperSemilattice(), particular1, particular2), 
+				extentIDs) ;
 		//build dag
 		DirectedAcyclicGraph<IConcept, IIsA> dag = new DirectedAcyclicGraph<IConcept, IIsA>(null, IsA::new, false);
 		dag.addVertex(particular1);
 		dag.addVertex(particular2);
 		dag.addVertex(genus);
-		dag.addVertex(truism);
 		dag.addVertex(ontologicalCommitment);
 		dag.addEdge(particular1, genus);
 		dag.addEdge(particular2, genus);
-		boolean genusIsTruism = genus.equals(truism);
-		if (!genusIsTruism)
-			dag.addEdge(genus, truism);
-		dag.addEdge(truism, ontologicalCommitment);
+		dag.addEdge(genus, ontologicalCommitment);
 		//build topo order
 		List<IConcept> topoOrder = new ArrayList<>(5);
 		topoOrder.add(particular1);
 		topoOrder.add(particular2);
 		topoOrder.add(genus);
-		if (!genusIsTruism)
-			topoOrder.add(truism);
 		topoOrder.add(ontologicalCommitment);
-		return new InvertedTree<IConcept, IIsA>(dag, ontologicalCommitment,	new HashSet<>(lattice.getParticulars()), topoOrder);
+		return new InvertedTree<IConcept, IIsA>(
+				dag, ontologicalCommitment,	
+				new HashSet<>(Arrays.asList(new IConcept[] {particular1, particular2})), 
+				topoOrder);
+	}
+	
+	private static IConcept copyConceptWithRestrictedExtent(IConcept concept, Set<Integer> restrictedExtentIDs) {
+		IConcept restrictedCopy;
+		Set<IConstruct> constructs = new HashSet<>();
+		for (IDenotation denotation : concept.getDenotations())
+			constructs.add(denotation.copy());
+		restrictedCopy = new Concept(constructs, restrictedExtentIDs, concept.iD());
+		restrictedCopy.setType(concept.type());
+		return restrictedCopy;
 	}
 
 }
